91e889652963449350a8a697b6735654698cf637
Camden Miller <91078110+KabutoDrip@users.noreply.github.com>
2022-09-21 22:26:38 -0600

    initial 3d basics

---
+++ index.js
@@ -0,0 +1,49 @@
+import * as THREE from './three.js-master/build/three.module.js'
+import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xdddddd)
+const loader = new GLTFLoader()
+
+
+loader.load('assets/scene.glb', function(glb){
+    console.log(glb)
+    const root = glb.scene;
+    root.scale.set(1,1,1)
+    scene.add(root)
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log('An error occured')
+})
+const ambientLight = new THREE.AmbientLight (0x404040,10);
+const directionalLight = new THREE.DirectionalLight(0xffffff,10);
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = true;
+scene.add(ambientLight)
+scene.add(directionalLight)
+
+const sizes ={
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
+camera.position.set(14,5,14);
+camera.rotation.y = 55/180*Math.PI;
+scene.add(camera)
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+renderer.gammOutput = true
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+}
+animate()
